e0d257b8539fbf839525f125122ceb28b3840b48
Trần Hữu Lộc <loctran0169@gmail.com>
2020-04-04 09:55:44 +0700

    create window

---
+++ RemakeCastleVania/Source.cpp
@@ -0,0 +1,11 @@
+#include <d3d9.h>
+#include <Windows.h>
+int WINAPI WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpStrCmdLine,
+	int nShowCmd)
+{
+	MessageBox(0, L"Welcome to Game Programming!", L"Hello DirectX", MB_OK);
+	return 0;
+}
